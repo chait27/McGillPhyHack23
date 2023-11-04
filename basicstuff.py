ffd29453fcb1d797e92e2b9e5edca8a4ea08e5ed
zipstyx <90870302+zipstyx@users.noreply.github.com>
2023-11-04 15:19:54 -0400

    Lattice class
    Finalized how indexing works. Lattice_vecs is a 3d array, where each 2d matrix slice represents the locations of all the sites for a particular particle in the unit cell. Similarly, spin_matrix is a 3d array where each row in each 2d slice represents the xyz spin of a site of one particular particle in the unit cell.

--- basicstuff.py
+++ basicstuff.py
@@ -1,4 +1,5 @@
 import numpy as np
+from typing import Tuple
 
 
 def randomSpin():
@@ -11,11 +12,11 @@ def exchangeEnergy(s1, s2, intMatrix):
 
 
 class UnitCell:
-    basisvec: tuple
-    sites: tuple
-    interactions: np.ndarray
+    basisvec: Tuple[np.ndarray]
+    sites: Tuple[np.ndarray]
+    interactions: list
 
-    def init(self, basisvec, sites):
+    def __init__(self, basisvec: Tuple[np.ndarray], sites: Tuple[np.ndarray]):
         self.basisvec = basisvec
         self.sites = sites
         self.interactions = []
@@ -26,25 +27,37 @@ class UnitCell:
 
 class Lattice:
     unitcell: UnitCell
-    size: tuple
-    lattice_vec: np.ndarray  # Nx2 matrix
-    n_sites: int
-    spin_matrix: np.ndarray  # Nx3 matrix
+    size: Tuple[int]
+    lattice_vec: np.ndarray  # n_sites x 2 x len(unitcell.sites) matrix
+    n_cells: int
+    spin_matrix: np.ndarray  # n_sites x 3 x len(unitcell.sites) matrix
 
-    def __init__(self, size: tuple, unitcell: UnitCell):
-        self.unitcell = UnitCell
+    def __init__(self,  unitcell: UnitCell, size: tuple):
+        self.unitcell = unitcell
         self.size = size
+        self.n_cells = size[0]*size[1]
+        self.lattice_vecs = np.zeros(
+            shape=(self.n_cells, 2, len(self.unitcell.sites)))
 
-        self.n_sites = size[0]*size[1]
+        # Assumes we have one unique site
+        for k1 in range(self.size[0]):
+            for k2 in range(self.size[1]):
+                for (k, site) in enumerate(self.unitcell.sites):
+                    self.lattice_vecs[k1+k2*size[1], :, k] = site + k1 * \
+                        self.unitcell.basisvec[0] + \
+                        k2*self.unitcell.basisvec[1]
 
-        self.lattice_vec = np.zeros(shape=(self.n_sites, ))
-
-        for k2 in range(self.size[1]):
-            for k1 in range(self.size[0]):
-                self.lattice_vec[k1+k2*size[1]] = k1*self.unitcell.basisvec[0] + \
-                    k2*self.unitcell.basisvec[1]
-
-        self.spin_matrix = np.array([randomSpin() for _ in self.n_sites])
+        self.spin_matrix = np.array(
+            [[randomSpin() for _ in range(self.n_cells)] for _ in self.unitcell.sites])
 
     def Hamiltonian(self):
         pass
+
+
+if __name__ == '__main__':
+    # Example
+    uc = UnitCell(basisvec=(np.array([1, 0]), np.array([0, 1])), sites=(
+        np.array([1, 1]), np.array([1, 1.5])))
+    L = Lattice(uc, size=(3, 3))
+
+    print(L.spin_matrix)
