9376f8bcdffff3430a87cb330d222eb458f5081a
Chaitanya Kumar Mahajan <chait.mahajan@mail.utoronto.ca>
2023-11-05 09:22:38 -0500

    plotting stuff

--- plotting.py
+++ plotting.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import mpl_toolkits.mplot3d.axes3d as p3
-import matplotlib.animation as animation
+from matplotlib.colors import Normalize 
 
 from basicstuff import *
+
 
 def plot_basic_unitcell(lattice: Lattice):
     uc = lattice.unitcell
@@ -18,12 +18,27 @@ def plot_basic_unitcell(lattice: Lattice):
     plt.title("Unit Cell")
     plt.show()
 
-def plot_lattice(lattice: Lattice):
-    pass
-        
 
-def plot_spins(lattice: Lattice):
-    uc = lattice.unitcell
+def plot_lattice(lattice: Lattice):
+    (k1, k2) = lattice.size
+    lattice_vec = lattice.lattice_vec  # n_cells x 2 x len(unitcell.sites) matrix
+    numCell = lattice.n_cells
+    spin_matrix = lattice.spin_matrix  # n_cells x 3 x len(unitcell.sites) matrix
+
+    numCell, _, numSites = spin_matrix.shape
+    color, s = iter(plt.cm.rainbow(np.linspace(0, 1, numSites))), 0
+    for site in range(numSites):
+        Xloc = [lattice_vec[i][0][site] for i in range(numCell)]
+        Yloc = [lattice_vec[i][1][site] for i in range(numCell)]
+        Zloc = [0 for i in range(numCell)]
+        c = next(color)
+        plt.scatter(Xloc, Yloc, color = c, label = 'Site ' + str(s) + " Spin")
+        s += 1
+    plt.legend()
+    plt.show()
+
+        
+def plot_spins3D(lattice: Lattice):
     (k1, k2) = lattice.size
     lattice_vec = lattice.lattice_vec  # n_cells x 2 x len(unitcell.sites) matrix
     numCell = lattice.n_cells
@@ -43,8 +58,6 @@ def plot_spins(lattice: Lattice):
         Xloc = [lattice_vec[i][0][site] for i in range(numCell)]
         Yloc = [lattice_vec[i][1][site] for i in range(numCell)]
         Zloc = [0 for i in range(numCell)]
-        #print(Xloc,Yloc,Zloc)
-        #X, Y, Z = np.meshgrid(Xloc, Yloc, Zloc)
 
         XSpin = [spin_matrix[i][0][site] for i in range(numCell)]
         YSpin = [spin_matrix[i][1][site] for i in range(numCell)]
@@ -52,6 +65,7 @@ def plot_spins(lattice: Lattice):
         
         c = next(color)
         ax.quiver(Xloc, Yloc, Zloc, XSpin, YSpin, ZSpin, length=0.5, normalize=True, color = c, label='Site ' + str(s) + " Spin")
+        #ax.scatter(Xloc, Yloc, Zloc, color = c)
         s += 1
     
     ax.set_xlabel('X')
@@ -65,3 +79,109 @@ def plot_spins(lattice: Lattice):
     plt.legend()
     plt.title("Spin Structure")
     plt.show()
+
+
+def plot_spinsXYProjection(lattice: Lattice):
+    (k1, k2) = lattice.size
+    lattice_vec = lattice.lattice_vec  # n_cells x 2 x len(unitcell.sites) matrix
+    numCell = lattice.n_cells
+    spin_matrix = lattice.spin_matrix  # n_cells x 3 x len(unitcell.sites) matrix
+
+    numCell, _, numSites = spin_matrix.shape
+    
+    if (k1 != numCell // k2):
+        print("Size Error")
+        print(k1, k2, numCell)
+        return None
+    
+    color, s = iter(plt.cm.rainbow(np.linspace(0, 1, numSites))), 0
+
+    for site in range(numSites):
+        Xloc = [lattice_vec[i][0][site] for i in range(numCell)]
+        Yloc = [lattice_vec[i][1][site] for i in range(numCell)]
+        Zloc = [0 for i in range(numCell)]
+
+        XSpin = np.array([spin_matrix[i][0][site] for i in range(numCell)])
+        YSpin = np.array([spin_matrix[i][1][site] for i in range(numCell)])
+        ZSpin = np.array([spin_matrix[i][2][site] for i in range(numCell)])
+
+        
+        c = next(color)
+        norm = np.sqrt(XSpin**2 + YSpin**2)
+        plt.quiver(Xloc, Yloc, XSpin/norm, YSpin/norm, color = c, label='Site ' + str(s) + " Spin")
+        #plt.scatter(Xloc, Yloc, color = c)
+        s += 1
+
+    plt.xlabel('X')
+    plt.xlim(0, k1)
+    plt.ylabel('Y')
+    plt.ylim(0, k2)
+    plt.title("Spin Structure")
+    plt.show()
+
+
+def plot_spinsXYProjectionColor_inprogress(lattice: Lattice):
+    (k1, k2) = lattice.size
+    lattice_vec = lattice.lattice_vec  # n_cells x 2 x len(unitcell.sites) matrix
+    numCell = lattice.n_cells
+    spin_matrix = lattice.spin_matrix  # n_cells x 3 x len(unitcell.sites) matrix
+
+    numCell, _, numSites = spin_matrix.shape
+    
+    if (k1 != numCell // k2):
+        print("Size Error")
+        print(k1, k2, numCell)
+        return None
+    
+    color, s = iter(plt.cm.rainbow(np.linspace(0, 1, numSites))), 0
+
+    massiveX, massiveY, massiveZ = [], [], []
+    massiveXSpin, massiveYSpin, massiveZSpin = [], [], []
+
+    for site in range(numSites):
+        Xloc = np.array([lattice_vec[i][0][site] for i in range(numCell)])
+        Yloc = np.array([lattice_vec[i][1][site] for i in range(numCell)])
+        Zloc = np.array([0 for _ in range(numCell)])
+
+        XSpin = np.array([spin_matrix[i][0][site] for i in range(numCell)])
+        YSpin = np.array([spin_matrix[i][1][site] for i in range(numCell)])
+        ZSpin = np.array([spin_matrix[i][2][site] for i in range(numCell)])
+
+        massiveX, massiveY, massiveZ = np.concatenate((massiveX, Xloc)), np.concatenate((massiveY, Yloc)), np.concatenate((massiveZ, Zloc))
+        massiveXSpin, massiveYSpin, massiveZSpin = np.concatenate((massiveXSpin, XSpin)), np.concatenate((massiveYSpin, YSpin)), np.concatenate((massiveZSpin, ZSpin))
+
+        c = next(color)
+        norm = np.sqrt(XSpin**2 + YSpin**2)
+        plt.quiver(Xloc, Yloc, XSpin/norm, YSpin/norm, color = 'black', label='Site ' + str(s) + " Spin")
+        s += 1
+
+    xgrid, ygrid = np.linspace(massiveX.min(), massiveX.max(), 100), np.linspace(massiveY.min(), massiveY.max(), 100)
+    #X, Y = np.meshgrid(xgrid, ygrid)
+    cmap = plt.get_cmap('coolwarm')
+    colors = cmap(massiveZSpin)
+    print(colors)
+    plt.imshow(massiveZSpin.reshape(k1, k2), cmap = cmap, vmin = massiveZSpin.min(), 
+                                    vmax = massiveZSpin.max(), 
+                                    extent=(xgrid.min(), xgrid.max(), ygrid.min(), ygrid.max()), 
+                                    origin='lower', interpolation='none', aspect='auto')
+    plt.colorbar(label='TODO')
+
+    
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    plt.show()
+    
+
+"""plt.contourf(X,Y,Z=massiveZSpin, extent=(xgrid.min(), xgrid.max(), ygrid.min(), ygrid.max()))
+    #Spin = np.sqrt(massiveXSpin**2 + massiveYSpin**2 + massiveZSpin**2)
+    #cmap = plt.get_cmap('viridis')
+    #norm = Normalize(vmin = Spin.min(), vmax = Spin.max())
+
+    #spinz_grid = np.interp(X, massiveX, Spin)
+    #colors = cmap(massiveZSpin)
+
+    #plt.imshow(colors, extent=(xgrid.min(), xgrid.max(), ygrid.min(), ygrid.max()), origin='lower', interpolation='bicubic', aspect='auto')
+    #plt.colorbar(label='TODO')
+
+    #plt.contourf(xgrid, ygrid, colors, levels=100, extend='both')
+    #plt.scatter(massiveX, massiveY, c=massiveZSpin, cmap=cmap)"""
